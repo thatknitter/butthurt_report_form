b3ff1ded8faec443aa2e5e55321e460b0e436564
thatknitter <kblewis.flt@gmail.com>
2014-11-06 15:36:27 -0600

    new cool stuff

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var $button = document.querySelector("button");
+	var $target = document.querySelector(".target");
+	var $phrases = ["No one cares.", "Well fold it up, put it in an envelope, and mail it to yo mama cause we don't care!", "You need help. With apathy. We don't care.", "::gives you some serious side-eye::", 
+	"The only thing more powerful than ignorance is apathy. Which I have. A lot of. For you.", "Go get some tissues and call the wahmbulance!", "Did you really fill this form out? Do you understand satire?"];
+	var $phrasesClone = $phrases.slice(0);
+	function randomPhrase(){
+		return $phrases[Math.floor(Math.random() * 7)];
+	}
+	$button.addEventListener('click', function(){
+		event.preventDefault();
+		$target.appendChild(createPTag(randomPhrase()));
+});
+});
+		
+function createPTag (text){
+	var docFragment = document.createDocumentFragment();
+		var $p = document.createElement('p');
+		$p.textContent = text;
+		docFragment.appendChild($p);
+		return docFragment;
+}
