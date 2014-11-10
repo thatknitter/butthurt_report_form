b6543ea293a0b148484a5568b5cb185490a29ba7
thatknitter <kblewis.flt@gmail.com>
2014-11-10 17:38:23 -0600

    finished

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
 	var $button = document.querySelector("button");
 	var $target = document.querySelector(".target");
-	var $phrases = ["No one cares.", "Well fold it up, put it in an envelope, and mail it to yo mama cause we don't care!", "You need help. With apathy. We don't care.", "::gives you some serious side-eye::", 
-	"The only thing more powerful than ignorance is apathy. Which I have. A lot of. For you.", "Go get some tissues and call the wahmbulance!", "Did you really fill this form out? Do you understand satire?"];
+	var insult = document.querySelector(".insult");
+	var $phrases = ["No one cares.", "Well fold it up, put it in an envelope, and mail it to yo mama cause we don't care!", "You need help. With apathy. We don't care.", "::gives you some serious side-eye::", "The only thing more powerful than ignorance is apathy. Which I have. A lot of. For you.", "Go get some tissues and call the wahmbulance!", "Did you really fill this form out? Do you understand satire?"];
 	var $phrasesClone = $phrases.slice(0);
-	function randomPhrase(){
-		return $phrases[Math.floor(Math.random() * 7)];
+	function randomPhrase() {
+		return $phrases[Math.floor(Math.random() * 9)];
 	}
-	$button.addEventListener('click', function(){
+
+
+	$button.addEventListener('click', function() {
+		$target.innerHTML = "";
 		event.preventDefault();
 		$target.appendChild(createPTag(randomPhrase()));
+	});
 });
-});
-		
-function createPTag (text){
+
+function createPTag(text) {
 	var docFragment = document.createDocumentFragment();
-		var $p = document.createElement('p');
-		$p.textContent = text;
-		docFragment.appendChild($p);
-		return docFragment;
+	var $p = document.createElement('p');
+	$p.textContent = text;
+	docFragment.appendChild($p);
+	return docFragment;
 }
